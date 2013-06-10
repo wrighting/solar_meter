6b83344dcbb306989fe018b0f626806381b20342
wrighting <tech@wrighting.org>
2013-06-10 14:44:06 +0200

    Add some processing to make into an easier to process CSV

--- scripts/download.sh
+++ scripts/download.sh
@@ -1,8 +1,12 @@
+#!/bin/sh
+
+PATH=/bin:/usr/bin
+export PATH
 COOKIES=cookies
 METER_NO=$1
 PASSWORD=$2
 
 ID=`curl -v -X POST --data "mtrnum=${METER_NO}&passcode=${PASSWORD}" -c ${COOKIES} http://www.ss4meteronline.co.uk/logValMob.asp 2>&1 | grep Location: | awk -F\& '{print $2}'`
 
-curl -v -b ${COOKIES} "http://www.ss4meteronline.co.uk/showMeterRdgs2.asp?${ID}&serv=Daily&gen="
-curl -v -b ${COOKIES} http://www.ss4meteronline.co.uk/saveCSV2.asp > readings.csv
+curl -b ${COOKIES} "http://www.ss4meteronline.co.uk/showMeterRdgs2.asp?${ID}&serv=Daily&gen=" > /dev/null 2>&1
+curl -b ${COOKIES} http://www.ss4meteronline.co.uk/saveCSV2.asp | tail -n +2 | sed -e 's/ \([TV]*\)/\1/g' 
