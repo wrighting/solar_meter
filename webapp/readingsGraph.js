fc1d0d0f40992465aba601da0c2a4c7f1835d983
wrighting <tech@wrighting.org>
2014-01-10 16:12:23 +0100

    Deal with changed date format

--- webapp/readingsGraph.js
+++ webapp/readingsGraph.js
@@ -1,11 +1,12 @@
 // Require all dependencies
 require([  "dojo/_base/lang", "dojo/dom-construct", "dojo/dom", "dojo/topic",
+		"dijit/layout/ContentPane", "dojox/layout/TableContainer",
 		"dojox/charting/widget/Legend","dojox/charting/action2d/Tooltip", "dojox/grid/DataGrid",
 		"dojo/data/ObjectStore", "dojo/store/DataStore", "dojo/store/Memory", "dojox/charting/Chart",
 		"dojox/charting/themes/Claro", "dojo/store/Observable",
 		"dojox/data/CsvStore", "dojox/charting/StoreSeries",
 		"dojox/charting/plot2d/Lines", "dojox/charting/axis2d/Default",
-		"dojo/domReady!" ], function(lang, domConstruct, dom, topic, Legend, Tooltip, DataGrid, ObjectStore, DataStore, Memory,
+		"dojo/domReady!" ], function(lang, domConstruct, dom, topic, ContentPane, TableContainer, Legend, Tooltip, DataGrid, ObjectStore, DataStore, Memory,
 		Chart, Claro, Observable, CsvStore, StoreSeries) {
 
 	// Create the data store
@@ -16,7 +17,9 @@ require([  "dojo/_base/lang", "dojo/dom-construct", "dojo/dom", "dojo/topic",
 
 	function timeStampToDate(timestamp) {
 		var dp = timestamp.split('/');
-		var d = new Date(dp[2].substr(0, 4), dp[1] - 1, dp[0]);
+		var year = parseInt(dp[2].substr(0,2));
+		year += 2000;
+		var d = new Date(year, dp[1] - 1, dp[0]);
 		return (d);
 	};
 	dataStore.comparatorMap = {};
@@ -45,7 +48,7 @@ require([  "dojo/_base/lang", "dojo/dom-construct", "dojo/dom", "dojo/topic",
 		outputStats(mstore, weekNo);
 	});
 	function calculateFIT(start, end) {
-		var units = start["reading"];
+		var units = start["weeklyTotal"];
 		if (end) {
 			units = end["total"] - start["total"];
 		}
@@ -55,11 +58,14 @@ require([  "dojo/_base/lang", "dojo/dom-construct", "dojo/dom", "dojo/topic",
 		return (qt.toFixed(2));
 	}
 	var lastValue = 0;
+	var lastWeeklyTotal = 0;
 	dstore.query().forEach(function(reading) {
 		var d = timeStampToDate(reading["TimeStamp"]);
 		//Only collect values for first day of week
-		if (d.getDay() == 1) {
+		//if (d.getDay() == 1) {
+			reading["year"] = d.getFullYear();
 			reading["weekOfYear"] = d.getWeek();
+			reading["dayOfWeek"] = d.getDay();
 			reading["weekNo"] = weekNo++;
 			var read = reading["IncTotalValue"];
 			// Account for first week
@@ -71,14 +77,42 @@ require([  "dojo/_base/lang", "dojo/dom-construct", "dojo/dom", "dojo/topic",
 			//Do our own calculation as using weekly totals not daily 
 			reading["reading"] = reading["total"] - lastValue;
 			lastValue = reading["total"];
+			reading["weeklyTotal"] = reading["total"] - lastWeeklyTotal;
+			if (d.getDay() == 1) {
+				lastWeeklyTotal = reading["total"];
+			}
 		// dstore.put(reading, { overwrite: true });
 			mstore.add(reading, { id: weekNo - 1 });
-		}
+		//}
 
 	}).then(function () {
 		topic.publish("weekLoaded");
 	});
 
+	var years = [ "2011", "2012", "2013", "2014" ];
+	function showWeeklyTotals(container, weekStore, weekNo, text) {
+		var count = 0;
+		
+		weekStore.query({weekOfYear: weekNo, dayOfWeek: 1}).forEach(function (previousYear) {
+			var d = timeStampToDate(previousYear["TimeStamp"]);
+			var year = d.getFullYear();
+			var y1 = parseInt(years[count]);
+			if (count == 0 && year != years[count]) {
+		//		domConstruct.create("div", { innerHTML: "" }, container);
+			}
+			count++;
+			var output = text;
+			output += year + ": ";
+			output += previousYear["weeklyTotal"].toFixed(2);
+			output += " (&pound; " + calculateFIT(previousYear) + ")";
+			//domConstruct.create("div", { innerHTML: text }, container);
+    			var cell = new ContentPane({
+      				content: output
+    			});
+			container.addChild(cell);
+		});
+		return text;
+	}
 	function outputStats(weekStore, weekNo) {
 
 		var quarter = weekStore.get(weekNo - 14);
@@ -86,14 +120,14 @@ require([  "dojo/_base/lang", "dojo/dom-construct", "dojo/dom", "dojo/topic",
 		var latest = weekStore.get(weekNo - 1);
 		var qTotal = latest["total"] - quarter["total"];
 		var node = dom.byId("totals");
-		//var w = domConstruct.create("div", { innerHTML: "This week:" + latest["reading"].toFixed(2) + " (&pound; " + calculateFIT(lastWeek,latest) + ")"}, node);
-		weekStore.query({weekOfYear: latest["weekOfYear"]}).forEach(function (previousYear) {
-			domConstruct.create("div", { innerHTML: "This week in " + previousYear["TimeStamp"].substr(6,4) +": " + previousYear["reading"].toFixed(2) + " (&pound; " + calculateFIT(previousYear) + ")" }, node);
-		});
-	
-		var l = domConstruct.create("div", { innerHTML: "Last week:" + lastWeek["reading"].toFixed(2) + " (&pound; " + calculateFIT(lastWeek) + ")"}, node);
+		var tbl = new TableContainer( {
+						cols: 3,
+						}, node);
+		showWeeklyTotals(tbl, weekStore, latest["weekOfYear"], "This week in ");
+		showWeeklyTotals(tbl, weekStore, lastWeek["weekOfYear"], "Previous week in ");	
+		
 		var qt = ((qTotal * 39.6) + ((qTotal/2) * 3.2)) / 100;
-		var q = domConstruct.create("div", { innerHTML: "This quarter:" + qTotal.toFixed(2) + " (&pound; " + calculateFIT(quarter,latest) + ")"}, node);
+		var q = domConstruct.create("div", { innerHTML: "This quarter :" + qTotal.toFixed(2) + " (&pound; " + calculateFIT(quarter,latest) + ")"}, node);
 //console.log(qTotal.toFixed(2));
 //console.log(results);
 	};
@@ -126,16 +160,15 @@ require([  "dojo/_base/lang", "dojo/dom-construct", "dojo/dom", "dojo/topic",
 		min : 0
 	});
 	var currentTotal = 0;
-	var years = [ "2011", "2012", "2013" ];
 
 	for (i = 0; i < years.length; i++) {
 		filter = "*" + years[i] + "*";
-		var year = { year: years[i], currentTotal: 0 };
+		var year = { year: years[i], currentTotal: currentTotal };
 		var filter = dojo.hitch(year, 
 			function(object) {
-				var dp = object["TimeStamp"].split('/');
-				var d = new Date(dp[2].substr(0, 4), dp[1] - 1, dp[0]);
-				if (d.getFullYear() == this.year) {
+				var d = timeStampToDate(object["TimeStamp"]);
+		//Only collect values for first day of week
+		if (d.getDay() == 1 && d.getFullYear() == this.year) {
 					return true;
 				} else {
 					return false;
@@ -144,19 +177,24 @@ require([  "dojo/_base/lang", "dojo/dom-construct", "dojo/dom", "dojo/topic",
 		var seriesValues = dojo.hitch(year,
 	function (item, store) {
 		var reading = parseFloat(item["total"]) - this.currentTotal;
-		this.currentTotal = parseFloat(item["total"]);
 
 		// Account for first week
 
 		if (reading <= 0) {
 			reading = parseFloat(item["reading"]);
 		}
+		
+		if (reading > 200) {
+			reading = 0;
+		}
+
+		this.currentTotal = parseFloat(item["total"]);
 
 		// let's create our object
 		var o = {
 			y : reading,
 			x : item["weekOfYear"],
-			tooltip : item["TimeStamp"].substr(0, 10) + " "
+			tooltip : item["TimeStamp"].substr(0, 8) + " "
 					+ reading.toFixed(2),
 			color : "red"
 		};
@@ -169,12 +207,14 @@ require([  "dojo/_base/lang", "dojo/dom-construct", "dojo/dom", "dojo/topic",
 			query: filter
 		}, seriesValues);
 		chart.addSeries("y" + i, series);
+		//This doesn't work as the series hasn't yet been filtered
+		currentTotal = year.currentTotal;
 	}
 	// Render the chart!
 	chart.render();
 
 	function timeStampFormatter (timestamp) {
-		return timestamp.substr(0,10);
+		return timestamp.substr(0,8);
 	}
 	var grid = new DataGrid({
 		store : dataStore,
